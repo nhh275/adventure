c054a10ba11b30ceed710faaea7a765bd33bc85a
Nelson Hearfield <2884923h@student.gla.ac.uk>
2026-06-01 16:23:19 +0100

    start

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import requests
 
 print("\nWelcome to your grand adventure!")
-playerName = input("What is your name, adventurer? ") #unsanitised
+playerName = input("What is your name, adventurer? ").title() # unsanitised 
+url = "https://www.dnd5eapi.co/api/2014/classes"
+response = requests.get(url)
+data = response.json()
+print("\n")
+for i in range(data['count']):
+    print(data['results'][i]['name'])
+
+chosenClass = str(input("\nWhich class takes your fancy today? ")).strip().lower()
+url = f"https://www.dnd5eapi.co/api/2014/classes/{chosenClass}"
+response = requests.get(url)
+classData = response.json()
+
+
+url = "https://www.dnd5eapi.co/api/2014/races"
+response = requests.get(url)
+data = response.json()
+print("\n")
+for i in range(data['count']):
+    print(data['results'][i]['name'])
+    
+chosenRace = str(input("\nWhich race are you? ")).strip().lower()
+url = f"https://www.dnd5eapi.co/api/2014/races/{chosenRace}"
+response = requests.get(url)
+raceData = response.json()
+
+print(f"\n{playerName} the mighty {raceData['name']} {classData['name']}! A wonderful choice.")
+
