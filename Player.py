bd1190bd705988d4ea8686a28b74284b7f61da11
Nelson Hearfield <2884923h@student.gla.ac.uk>
2026-06-01 18:54:14 +0100

    first choice

---
+++ Player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name, player_class, race, equipment, proficiencies):
+        self.name = name
+        self.player_class = player_class
+        self.race = race
+        self.equipment = equipment
+        self.proficiencies = proficiencies
+    
+    
